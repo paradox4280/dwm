9ee3313b6ad3952bf7f36ea43450d01b735b889f
paradox4280 <zmf5c2fi@pm.me>
2023-07-30 11:20:14 +0000

    config: Add keybind

--- config.def.h
+++ config.def.h
@@ -70,12 +70,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browser[] = { "firefox", NULL };
+static const char *file_manager[] = { "pcmanfm", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
     { ALTKEY,                       XK_f,      spawn,          {.v = browser } },
+    { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = file_manager } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
